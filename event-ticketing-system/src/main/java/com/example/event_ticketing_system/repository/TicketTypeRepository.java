52fd377c2b1d68791bc680245abc2debfd18655c
kalmk <juyeongk365@csu.fullerton.edu>
2026-03-03 10:13:24 -0800

    fix

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/TicketTypeRepository.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/TicketTypeRepository.java
@@ -1,9 +1,11 @@
 package com.example.event_ticketing_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.event_ticketing_system.entity.TicketType;
 
+@Repository
 public interface TicketTypeRepository extends JpaRepository<TicketType, Integer>{
     
 }
