4b01c943a80f5a69b3e947e518507ba7b2809a05
kalmk <juyeongk365@csu.fullerton.edu>
2026-03-03 09:36:35 -0800

    wip

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/dto/BookingResponseDTO.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/dto/BookingResponseDTO.java
@@ -1,5 +1,21 @@
 package com.example.event_ticketing_system.dto;
 
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponseDTO {
-    
+    private String bookingReference;
+    private LocalDateTime bookingDate;
+    private String paymentStatus;
+    private String attendeeName;
+    private String eventTitle;
+    private String ticketTypeName;
+    private BigDecimal price;
 }
--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/service/BookingService.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/service/BookingService.java
@@ -1,8 +1,27 @@
 package com.example.event_ticketing_system.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.event_ticketing_system.entity.Booking;
+import com.example.event_ticketing_system.repository.BookingRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class BookingService {
-    
+    private final BookingRepository bookingRepository;
+
+    @Transactional
+    public Booking bookTicket(Integer attendeeId, Integer ticketTypeId) {
+        // TO DO:
+        // IMPLEMENT LATER
+
+        Booking booking = new Booking();
+
+        // TO DO:
+        // IMPLEMENT LATER
+        return bookingRepository.save(booking);
+    }
 }
