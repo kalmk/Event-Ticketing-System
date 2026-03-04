56d3b48ae5889026033e518035436ed13ea72a74
Ju Yeong Kim <111930167+kalmk@users.noreply.github.com>
2026-03-04 15:59:47 -0800

    added venue

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/controller/VenueController.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/controller/VenueController.java
@@ -1,12 +1,27 @@
 package com.example.event_ticketing_system.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.event_ticketing_system.entity.Venue;
+import com.example.event_ticketing_system.service.VenueService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/venues")
+@RequiredArgsConstructor
 public class VenueController {
-   
+    private final VenueService venueService;
 
-    
+    @PostMapping
+    public ResponseEntity<Venue> createVenue(@RequestBody Venue venue) {
+        Venue created = venueService.createVenue(venue);
+
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
 }
--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/service/VenueService.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/service/VenueService.java
@@ -1,8 +1,20 @@
 package com.example.event_ticketing_system.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.event_ticketing_system.entity.Venue;
+import com.example.event_ticketing_system.repository.VenueRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class VenueService {
-    
+    private final VenueRepository venueRepository;
+
+    @Transactional
+    public Venue createVenue(Venue venue) {
+        return venueRepository.save(venue);
+    }
 }
